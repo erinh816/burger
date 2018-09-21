450f2949128594c7723c7c6eec99a0de80ead6c0
Silu Hu <silu@Silus-MacBook-Pro.local>
2018-09-21 18:39:08 -0400

    add basic

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+  INSERT INTO burgers (burger_name, devoured) VALUES ("Umami Burgers", false);
+  INSERT INTO burgers (burger_name, devoured) VALUES ("Pug Burger", false);
+  INSERT INTO burgers (burger_name, devoured) VALUES ("Minetta Burger", false);
